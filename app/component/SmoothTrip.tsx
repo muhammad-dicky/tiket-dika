a4a24e7cfeab5e7f059e9833526a487f48af0bd2
Dicky <muhammad.dickynn@gmail.com>
2023-12-06 13:52:27 +0700

    Refactor SmoothTrip and SmoothTripInfo components

--- app/component/SmoothTrip.tsx
+++ app/component/SmoothTrip.tsx
@@ -8,12 +8,12 @@ import React from 'react'
 
 export default function SmoothTrip () {
     return ( 
-<div className="p-10 lg:px-36 ">
-<p className="font-bold text-3xl">Smooth trip, zero worry!</p>
-<p className="text-sm">Avoid losses due to trip cancellations or rescheduling, booking errors, and more.</p>
-{/* <img src="/card1.png"/> */}
-<Image className="p-20" src={Smooth} alt={"wasd"}/>
+<div className="p-10 lg:px-36">
+  <p className="font-bold text-3xl">Smooth trip, zero worry!</p>
+  <p className="text-sm">Avoid losses due to trip cancellations or rescheduling, booking errors, and more.</p>
+  <Image className="md:p-20 pt-10" src={Smooth} alt={"wasd"} />
 </div>
+
      );
 }
  
--- app/component/SmoothTripInfo.tsx
+++ app/component/SmoothTripInfo.tsx
@@ -9,70 +9,64 @@ import Kiri1 from '../../public/kiri1.png'
 export default function SmoothTripInfo() {
   return (
     <>
-    <div className='p-10 flex justify-evenly'>
-        <div>
-        <Image className='w-96' src={Kiri} alt={'wasd'} />
-
-        </div>
-        <div className='flex flex-col space-y-5'>
-            <div className='text-3xl font-bold'>
-            Book Tickets and Plan Your Holiday with Ease
-            </div>
-
-            <div className='text-base font-bold'>
-            Easy to Buy Tickets and Hotels.
-            </div>
-            <div>
-            Buy flight tickets, hotels, trains, car rental, also tickets for event and attraction immediately on tiket.com app and website.
-            </div>
-
-            <div className='text-base font-bold'>
-            Various Top Products. 
-            </div>
-            <div>
-            Get various airlines with complete routes around the world and find many hotels in Asia. You also can get train tickets, book car rental, and entertaiment tickets.
-            </div>
-
-
-
-        </div>
+    <div className='p-10 flex flex-col md:flex-row md:justify-evenly'>
+  <div className='mb-8 md:mb-0 md:mr-8'>
+    <Image className='w-full md:w-96' src={Kiri} alt={'wasd'} />
+  </div>
+  <div className='flex flex-col space-y-5'>
+    <div className='text-3xl font-bold'>
+      Book Tickets and Plan Your Holiday with Ease
     </div>
 
-
-    <div className='p-10 flex justify-evenly'>
-        
-        <div className='flex flex-col space-y-5'>
-            <div className='text-3xl font-bold'>
-            Plenty of Promo and Benefits, Save Even More!
-            </div>
-
-            <div className='text-base font-bold'>
-            Get Special Promo & Discount! 
-            </div>
-            <div>
-            Find discounts for your favorite flights and hotels to save more for your upcoming vacation! Make your dream vacation come true with a special promo provided by tiket.com. There is no reason to delay your vacation!
-            </div>
-
-            <div className='text-base font-bold'>
-            Tiket Rewards Benefits.
-            </div>
-            <div>
-            Enjoy the Tiket Rewards benefits in Tiket Points that you can redeem with discounts.
-            </div>
-
-           
-
-           
-        </div>
-
-        <div>
-        <Image width={800} src={Kanan} alt={'wasd'} />
-
-        </div>
+    <div className='text-base font-bold'>
+      Easy to Buy Tickets and Hotels.
+    </div>
+    <div>
+      Buy flight tickets, hotels, trains, car rental, also tickets for events and attractions immediately on tiket.com app and website.
     </div>
 
+    <div className='text-base font-bold'>
+      Various Top Products.
+    </div>
+    <div>
+      Get various airlines with complete routes around the world and find many hotels in Asia. You can also get train tickets, book car rental, and entertainment tickets.
+    </div>
+  </div>
+</div>
 
-    <div className='p-10 flex justify-evenly'>
+
+<div className='p-10 flex flex-col-reverse items-center md:flex-row md:justify-evenly'>
+  
+
+  <div className='flex flex-col space-y-5 text-center md:text-left'>
+    <div className='text-3xl font-bold'>
+      Plenty of Promo and Benefits, Save Even More!
+    </div>
+
+    <div className='text-base font-bold'>
+      Get Special Promo & Discount!
+    </div>
+    <div>
+      Find discounts for your favorite flights and hotels to save more for your upcoming vacation! Make your dream vacation come true with a special promo provided by tiket.com. There is no reason to delay your vacation!
+    </div>
+
+    <div className='text-base font-bold'>
+      Tiket Rewards Benefits.
+    </div>
+    <div>
+      Enjoy the Tiket Rewards benefits in Tiket Points that you can redeem with discounts.
+    </div>
+  </div>
+
+  <div className='mr-20 mb-8 md:mb-0 md:mr-8'>
+    <Image width={800} src={Kanan} alt={'wasd'} />
+  </div>
+</div>
+
+
+   
+
+    <div className='p-10 flex flex-col md:flex-row justify-evenly'>
     <div>
         <Image width={800} src={Kiri1} alt={'wasd'} />
 
